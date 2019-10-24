a35c65d1fe5d602446667641c33241885aa16b23
steve8013 <stevemaxwel@outlook.com>
2019-10-25 00:03:47 +0800

    Preview of custom widget

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -17,4 +17,4 @@ CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
 CMS.registerPreviewTemplate('history', HistoryPagePreview);
-CMS.registerWidget('javascriptCustomWidget', JavascriptCustomWidget);
+CMS.registerWidget('javascriptCustomWidget', JavascriptCustomWidget, ({ value }) => value);
